2a26e9ff72edb95eea54a18cebc34eb8efe3cbcd
batuhanbaybas <b.baybas@gmail.com>
2022-02-25 21:48:14 +0300

    add json body input

--- src/Components/RequestBody/RequestBody.js
+++ src/Components/RequestBody/RequestBody.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const RequestBody = () => {
-  return (
-    <div>RequestBody</div>
-  )
+    return (
+        <div className='mt-10'>
+            <div className='text-xl'> JSON </div>
+            <textarea rows={8} className='p-3 border outline-none border-cyan-900 w-full ' />
+        </div>
+    )
 }
 
 export default RequestBody
