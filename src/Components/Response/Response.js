38619df865599deb2fa28bad51383e9027807f39
batuhanbaybas <b.baybas@gmail.com>
2022-02-26 19:05:44 +0300

    show results

--- src/Components/Response/Response.js
+++ src/Components/Response/Response.js
@@ -4,7 +4,13 @@ import { resData } from '../../Bloc/fetchData_Bloc'
 const Response = () => {
   const data = resData.use()
   return (
-    <div className='mt-10'>{JSON.stringify(data)}</div>
+    <div className='mt-10'>
+      <div>Response</div>
+      <div className='mt-5'>
+      {data.length > 0 &&JSON.stringify(data)}
+
+      </div>
+      </div>
   )
 }
 
