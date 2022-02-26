86a837fb6719a1f6cc20020616a5b734a95c7b3d
batuhanbaybas <b.baybas@gmail.com>
2022-02-26 23:33:15 +0300

    ...

--- src/Components/Response/Response.js
+++ src/Components/Response/Response.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { resData } from '../../Bloc/fetchData_Bloc'
+import { resData, resError } from '../../Bloc/fetchData_Bloc'
 
 const Response = () => {
   const data = resData.use()
@@ -9,20 +9,21 @@ const Response = () => {
   }
   return (
     <div className='py-10'>
+
       <div className='cursor-pointer' onClick={handleOpen}>Response</div>
-      
       {
         open &&
         <div className='mt-5'>
-          {data &&
-            <div className='bg-zinc-700 text-white p-10'>
-              <pre id="json">
-                {JSON.stringify(data, null, 2)}
-              </pre>
-            </div>
+          {
+            data && (
+              <div className='bg-zinc-700 text-white p-10'>
+                <pre id="json">
+                  {JSON.stringify(data, null, 2)}
+                </pre>
+              </div>
+            )
           }
         </div>
-
       }
     </div>
   )
